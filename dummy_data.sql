fbc1805cf101121d9e7ce164e74afcfcde5734c4
Hayden Panike <hpanike@gmail.com>
2014-12-08 21:18:25 -0700

    fix schema for multivalued name attributes & add dummy data

---
+++ dummy_data.sql
@@ -0,0 +1,17 @@
+USE reloading;
+
+INSERT INTO Bullet (
+VALUES (0, .308, "SMK BTHP 168", "BTHP", "SMK", 168, 0.462, 0.35, 100, "Copper, Lead")
+);
+
+INSERT INTO Casing (
+VALUES (0, .308, NULL, 5, ".308 Winchester", 100, .35, "LR")
+);
+
+INSERT INTO Powder (
+VALUES (0, "IMR 4096", "Non Magnum", "Medium", 454, .0658)
+);
+
+INSERT INTO Primer (
+VALUES (0, "CCI 200", "CCI", "LR", 1000, 0.028)
+);
