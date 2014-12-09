cdb7f6ebb6fd931050b407edcf2e18b2ee1f970e
Hayden Panike <hpanike@gmail.com>
2014-12-09 08:27:08 -0700

    add inital notes on triggers

---
+++ triggers.sql
@@ -0,0 +1,5 @@
+USE reloading;
+
+NOTE: Need trigger to update cost per bullet on update and insert on Recipes table.
+
+NOTE: Need trigget to update amount available on update and insert on Recipes table, or Powder table, or Bullets table, or Casings table, or Primers table.
